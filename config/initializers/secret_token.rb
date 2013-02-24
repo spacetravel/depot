43ac77f754c887b4e9e4b025d7b08e7d4434e824
Spacetravel Øyvind Dahl <spacetravel.no@gmail.com>
2013-02-24 22:26:54 +0100

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '85e2437296c62d1cf4e8a5ae957b41ae78c6157a67e262f29b975a1de2e113a5419b367e911538e7f85ff1fdd27d2233afd9dd3518b6d73f30072cdc2bc0fc49'
